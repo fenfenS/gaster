95c3f95ccd8f80ede2c761cc87b456502c9da1ef
fenfenS <104295404+fenfenS@users.noreply.github.com>
2025-03-11 16:56:22 +0800

    2.1

--- gaster.c
+++ gaster.c
@@ -1312,7 +1312,7 @@ gaster_checkm8(usb_handle_t *handle) {
 				puts("Stage: PATCH");
 				ret = checkm8_stage_patch(handle);
 				stage = STAGE_RESET;
-				exit(0);2
+				exit(0);
 			}
 			if(ret) {
 				puts("ret: true");
